b5a9cdc72cead33b3585a7cde80cb19ea803d6ca
Leslie Zhai <xiangzhai83@gmail.com>
2016-11-15 14:10:42 +0800

    Add missing source

---
+++ keyframes.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "libKeyFrame.h"
+
+int main(int argc, char *argv[]) 
+{
+    char *src = NULL;
+    char *out = NULL;
+    int width, height;
+
+    if ((src = argv[1]) == NULL || (out = argv[2]) == NULL) {
+        printf("Usage: keyframes /src/isoft.avi /out 320(Optional) 240(Optional)\n");
+        return 0;
+    }
+
+    if (access(out, W_OK) == -1)
+        out = "/tmp";
+
+    width = argv[3] ? atoi(argv[3]) : 0;
+    height = argv[4] ? atoi(argv[4]) : 0;
+    findKeyFrame(src, out, width, height);
+
+    return 0;
+}
